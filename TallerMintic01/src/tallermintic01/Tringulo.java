35299f0ced3837f4e08fa34cd3790e8c83cedfaf
Cristian David Quizá Pamplona <cquizapa@contratista.epm.co>
2022-06-20 20:11:47 -0500

    Creacion de repositorio compartido

---
+++ TallerMintic01/src/tallermintic01/Tringulo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tallermintic01;
+import java.util.Scanner;
+/**
+ *
+ * @author cristian.quiza
+ */
+public class Tringulo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Definir el valor que tendra tringulo         
+        System.out.println("          *           ");
+        System.out.println("       *  *  *        ");
+        System.out.println("    *  *  *  *  *     ");
+        System.out.println(" *  *  *  *  *  *  *  ");
+    }    
+}
