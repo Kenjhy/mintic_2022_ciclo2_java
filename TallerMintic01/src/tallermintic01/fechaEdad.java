713d6b8a4d72bd4deae89025153c645e8255f968
Efrain290493 <101163786+Efrain290493@users.noreply.github.com>
2022-06-20 21:26:18 -0500

    Ejercicio 4

--- TallerMintic01/src/tallermintic01/fechaEdad.java
+++ TallerMintic01/src/tallermintic01/fechaEdad.java
@@ -9,6 +9,24 @@ package tallermintic01;
  *
  * @author cristian.quiza
  */
+import java.util.Scanner;
+
 public class fechaEdad {
+    public static void main(String[] args) {
+        
+        int anioA = 0;
+        int anioN = 0;
+        
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.print("Ingrese año actual: "); anioA = teclado.nextInt();
+        System.out.print("Ingrese año de nacimiento: "); anioN = teclado.nextInt();
+        
+        int edad  = (anioA - anioN);
+        
+        System.out.println("Su edad es: "+ edad + " años");
+        
+        
+    }
     
 }
