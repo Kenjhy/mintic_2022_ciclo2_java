364f7ffb0570bb37bd4c799a043dd731446edb30
Cristian David Quizá Pamplona <cquizapa@contratista.epm.co>
2022-06-27 21:47:01 -0500

    Cristian correjido

--- Semana2Talleres/src/tallermintic01/NumerosOperador8.java
+++ Semana2Talleres/src/tallermintic01/NumerosOperador8.java
@@ -13,32 +13,37 @@ import java.util.Scanner;
  */
 public class NumerosOperador8 {
     public static void main(String[] args) {
+        //scanea lo que el dato del usuario
         Scanner Teclado= new Scanner(System.in);
-        System.out.println("Escribre dos numeros: ");
+        System.out.println("Escriba dos numeros: ");
         
         double n1 = Teclado.nextDouble();
         double n2 = Teclado.nextDouble();
-        double respueta;
+        double respuesta;
         
-        System.out.println("Escribre el operador aritmetico +, -, *, /: ");
+        System.out.println("Escriba el operador aritmetico +, -, *, /: ");
         
         char op = Teclado.next().charAt(0);
-    
+            
         switch (op) {
             
-            case '+': respueta = n1 + n2;
-                System.out.println("la suma de los numeros es: " + respueta);
+            case '+': respuesta = n1 + n2;
+                System.out.println("la suma de los numeros es: " + respuesta);
                 break;
-            case '-': respueta = n1 - n2;
-                System.out.println("la resta de los numeros es: " + respueta);
+            case '-': respuesta = n1 - n2;
+                System.out.println("la resta de los numeros es: " + respuesta);
                 break;
-            case '*': respueta = n1 * n2;
-                System.out.println("la multiplicacion de los numeros es: " + respueta);
+            case '*': respuesta = n1 * n2;
+                System.out.println("la multiplicacion de los numeros es: " + respuesta);
                 break;
-            case '/': respueta = n1 / n2;
-                System.out.println("la divicion de los numeros es: " + respueta);
+            case '/': respuesta = n1 / n2;
+                System.out.println("la division de los numeros es: " + respuesta);
+                if (n2 == 0) {
+                    System.out.println("OVERFLOV");
+                }
                 break;
-            default: System.out.println("Operador inexistente");;
+            
+            default: System.out.println("NO reconoce el operador");
         }
         
     }
