4acd9c107fe071ded96dfd22ae05912eff96e07d
Fabian Araque <jeisonfabian.araque@gmail.com>
2022-06-27 20:28:48 -0500

    Ejercicios 4 y 5

--- Semana2Talleres/src/tallermintic01/DeducirOrdenNumeros.java
+++ Semana2Talleres/src/tallermintic01/DeducirOrdenNumeros.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tallermintic02;
+package tallermintic01;
 
 import java.util.Scanner;
 /**
