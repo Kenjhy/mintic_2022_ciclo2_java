2048cbc6ae4319c3d5d3281bcd4a3a219a3218f3
Efrain290493 <101163786+Efrain290493@users.noreply.github.com>
2022-07-06 16:01:28 -0500

    Clases Triangulo y Rectangulo

--- Reto1/src/Clases/Rectangulo.java
+++ Reto1/src/Clases/Rectangulo.java
@@ -5,10 +5,23 @@
  */
 package Clases;
 
-/**
- *
- * @author cristian.quiza
- */
+import java.util.Scanner;
+
+
 public class Rectangulo {
-    
+    public static void main(String [] args){
+         
+        double base, altura, resultado;     
+        Scanner obj = new Scanner(System.in);
+         
+        System.out.print("Ingrese base:\t");
+        base = obj.nextDouble();
+         
+        System.out.print("Ingrese altura:\t");
+        altura = obj.nextDouble();
+         
+        resultado = (base*altura);
+         
+        System.out.print("El area del Rectangulo es:\t"+resultado);
+    }  
 }
--- Reto1/src/Clases/Triangulo.java
+++ Reto1/src/Clases/Triangulo.java
@@ -1,14 +1,32 @@
-/*
+    /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package Clases;
 
+import java.util.Scanner;
+
 /**
  *
  * @author cristian.quiza
  */
 public class Triangulo {
-    
+    public static void main(String[] args) {
+         double base, altura, resultado;     
+        Scanner obj = new Scanner(System.in);
+         
+        System.out.print("Ingrese base:\t");
+        base = obj.nextDouble();
+         
+        System.out.print("Ingrese altura:\t");
+        altura = obj.nextDouble();
+         
+        resultado = ((base*altura)/2);
+         
+        System.out.print("El area del Triangulo es:\t"+resultado);
+    }
 }
+    
+    
+
