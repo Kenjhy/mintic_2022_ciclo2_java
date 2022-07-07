d96db5cbea111f5675bceebcc2f10c9ab1c3c32d
dvargasp <dvargasp@intergrupo.com>
2022-07-07 17:23:26 -0500

    config base triangulo 2

--- Reto1/src/Clases/Rectangulo.java
+++ Reto1/src/Clases/Rectangulo.java
@@ -7,7 +7,6 @@ package Clases;
 
 import java.util.Scanner;
 
-<<<<<<< HEAD
 /**
  *
  * @author cristian.quiza
@@ -43,23 +42,4 @@ public class Rectangulo extends Figura{
         perimetro = (2*alto) + (2*ancho);
     }
     
-=======
-
-public class Rectangulo {
-    public static void main(String [] args){
-         
-        double base, altura, resultado;     
-        Scanner obj = new Scanner(System.in);
-         
-        System.out.print("Ingrese base:\t");
-        base = obj.nextDouble();
-         
-        System.out.print("Ingrese altura:\t");
-        altura = obj.nextDouble();
-         
-        resultado = (base*altura);
-         
-        System.out.print("El area del Rectangulo es:\t"+resultado);
-    }  
->>>>>>> 2048cbc6ae4319c3d5d3281bcd4a3a219a3218f3
 }
--- Reto1/src/Clases/Triangulo.java
+++ Reto1/src/Clases/Triangulo.java
@@ -12,33 +12,7 @@ import java.util.Scanner;
  * @author cristian.quiza
  */
 public class Triangulo {
-<<<<<<< HEAD
     
-    
-    public double jdfnksjdfn(){
-     int Radio;     
-        System.out.println("Ingresa el tamaño del radio: ");
-        Scanner radio = new Scanner(System.in);
-        Radio = radio.nextInt();
-        double Area = Math.PI * Math.pow(Radio, 2) ; 
-        System.out.println("El área del circulo es de " + Area);
-        return Area;
-=======
-    public static void main(String[] args) {
-         double base, altura, resultado;     
-        Scanner obj = new Scanner(System.in);
-         
-        System.out.print("Ingrese base:\t");
-        base = obj.nextDouble();
-         
-        System.out.print("Ingrese altura:\t");
-        altura = obj.nextDouble();
-         
-        resultado = ((base*altura)/2);
-         
-        System.out.print("El area del Triangulo es:\t"+resultado);
->>>>>>> 2048cbc6ae4319c3d5d3281bcd4a3a219a3218f3
-    }
 }
     
     
