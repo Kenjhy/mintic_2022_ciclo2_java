4441e6300873f19ff96363ed87e809fec9e64024
Cristian David Quizá Pamplona <cquizapa@contratista.epm.co>
2022-07-17 22:55:24 -0500

    Proyecto Figuras

---
+++ Semana 4/Semana4Reto4/Semana4Reto4/src/Clases/Circulo.java
@@ -0,0 +1,37 @@
+package Clases;
+//dd
+import java.util.Scanner;
+
+public class Circulo extends Figura {
+    private double radio;
+    
+    public Circulo(double radio, String nombre, double area, double perimetro) {
+        super(nombre, area, perimetro);
+        this.radio = radio;
+        calcularArea();
+        calcularPerimetro();
+    } 
+    
+        //Setear y getear todos los atributos
+    public void setRadio(double radio) {    
+        System.out.println("Ingresa el tamaño del radio: ");
+        Scanner scanner = new Scanner(System.in);
+        this.radio = scanner.nextDouble();
+    }
+
+    @Override
+    public void calcularArea() {
+        area = Math.PI * Math.pow(radio, 2);
+    } 
+
+    @Override
+    public void calcularPerimetro() {
+        perimetro = 2 * Math.PI * radio;
+    }      
+    
+    @Override
+    public String getTipoFigura() {
+        return "FIGURA CIRCULO";
+    }
+    
+}
--- Semana 4/Semana4Reto4/Semana4Reto4/src/Vistas/frmFiguras.java
+++ Semana 4/Semana4Reto4/Semana4Reto4/src/Vistas/frmFiguras.java
@@ -1586,7 +1586,7 @@ public class frmFiguras extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                 new frmFiguras().setVisible(true);
+                new frmFiguras().setVisible(true);
             }
         });
     }
